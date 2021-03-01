2239b685a6ca468e594411760da4cf6eddccd4d7
Carlos Oliva <olivam@protonmail.com>
2021-02-28 17:23:09 -0800

    Agregué más código base

--- ghc/__main__.py
+++ ghc/__main__.py
@@ -5,10 +5,11 @@ import ghc.competition as cmp
 
 
 if __name__ == '__main__':
-  in_filename, out_filename = ghc.get_args(cmp.PROBLEM_NAME, cmp.YEAR)
   in_desc = [
     # llenar con la descripcion de los datos de entrada
   ]
+  in_filename, out_filename = ghc.get_args(cmp.PROBLEM_NAME, cmp.YEAR)
   in_data = ghc.read_input_file(in_filename, in_desc)
   # procesar los datos de entrada segun el problema de la competencia
-  # escribir los resultados en un archivo almacenado en la ruta out_filename
+  out_data = None
+  cmp.write_output_file(out_filename, out_data)
--- ghc/competition.py
+++ ghc/competition.py
@@ -3,3 +3,17 @@
 
 PROBLEM_NAME = '<problem>'   # llenar con el nombre del problema
 YEAR = '<year>'              # llenar con el año de la competencia
+
+
+
+
+# Escribe los datos contenidos en data en un archivo de texto en la direccion
+# especificada por filename.
+# filename (str):   ruta completa del archivo que sera creado
+# data (any):       los datos a almacenar en el archivo
+def write_output_file(filename, 
+                      data):
+  file = open(filename, 'wt')
+  # escribir los datos de salida en el archivo
+  # llenar con la logica especifica del problema de la competencia
+  file.close()
